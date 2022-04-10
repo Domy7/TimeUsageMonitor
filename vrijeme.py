329fea6b758d60dbf734c0e5e3b2c8d488235b90
Antonio Juričić <76628050+antoniojuricic@users.noreply.github.com>
2022-04-10 11:41:48 +0200

    Spremanje vrijednosti u bazu podataka
    Inicijalizacija SQLite baze podataka i funkcije za spremanje vrijednosti o vremenu koristenja aplikacija po danima

--- vrijeme.py
+++ vrijeme.py
@@ -3,21 +3,27 @@ import psutil
 import time
 import win32process
 import json
+import os
+from datetime import date
+import sqlite3
 
-process_time={}
+process_time = {}
 timestamp = {}
 
+pocetnoVrijeme = time.time()
+today = str(date.today())
+
+#konfiguracija baze podataka
+con = sqlite3.connect('data.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS usage
+                (date text, app text, time integer)''')
 
 def petSekundi(oldtime):
-    
     if time.time() - oldtime > 5:
         return True
     
     return False
-
-    
-
-pocetnoVrijeme = time.time()
 
 while True:
     current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
@@ -32,11 +38,18 @@ while True:
 #ukupnoVr + trenutnoVr - vrijeme kada je otvorena apk zapisno u timestampu    
     process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
     
-#svakih 5 sekundi spremi podatke u json file    
+#svakih 5 sekundi azuriraj podatke u bazi    
     if petSekundi(pocetnoVrijeme):
-        with open('data.json', 'w') as fp:
-            json.dump(process_time, fp)
+        for open_app in process_time:
+            cur.execute('''SELECT * FROM usage WHERE date=? AND app=?''',(today, open_app))
+            result = cur.fetchone()
+            if result:
+                cur.execute('''UPDATE usage SET time=? WHERE date=? AND app=?''',(result[2]+5, today, open_app))
+            else:
+                cur.execute('''INSERT INTO usage(date,app,time) 
+                                VALUES (?,?,?)''',(today, open_app, process_time[open_app]))
+            con.commit()
+
         pocetnoVrijeme = time.time();
     
     print(process_time)
-
